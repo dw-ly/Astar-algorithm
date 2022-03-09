6b226edaf40ebf9eb2b95e46eadb75dcab670b10
root <root@localhost.localdomain>
2022-03-09 15:27:52 +0800

    cmake

---
+++ build/lib/CMakeFiles/libAStar.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/usr/Document/Astar-algorithm/lib/A*.cpp" "/usr/Document/Astar-algorithm/build/lib/CMakeFiles/libAStar.dir/A*.cpp.o"
+  "/usr/Document/Astar-algorithm/lib/gridMap.cpp" "/usr/Document/Astar-algorithm/build/lib/CMakeFiles/libAStar.dir/gridMap.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
